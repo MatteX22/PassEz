c5a26acc7650ec3cc0308606ddd156b691901cab
MatteX22 <64346361+MatteX22@users.noreply.github.com>
2026-01-18 00:31:39 +0100

    Test finale 1

--- sw.js
+++ sw.js
@@ -7,6 +7,13 @@ const FILES = [
   "sw.js"
 ];
 
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
