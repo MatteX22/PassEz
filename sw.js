9c6270c4498b9c20a6573dbe3143edf464285a4e
MatteX22 <64346361+MatteX22@users.noreply.github.com>
2026-01-18 00:30:07 +0100

    Risoluzione finale sw

--- sw.js
+++ sw.js
@@ -7,24 +7,19 @@ const FILES = [
   "sw.js"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
-  );
-  self.skipWaiting();
-});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      if (r) return r;
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((k) => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      )
-    )
+      // se è una navigazione (pagina) usa index.html
+      if (e.request.mode === "navigate") {
+        return caches.match("index.html");
+      }
+
+      return fetch(e.request);
+    })
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
